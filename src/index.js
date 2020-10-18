89a6bf909b6a41427a1e3aca4017f20b81160400
Terashima <yamanogomora@gmail.com>
2020-10-18 17:12:56 +0900

    Create a Register From

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css'
 
 import App from './components/App';
 import Login from './components/Auth/Login';
