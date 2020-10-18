f2f5afec7fd58938864726bc137db257348ae945
Terashima <yamanogomora@gmail.com>
2020-10-18 20:20:36 +0900

    Create Login Form and Functionality

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -118,7 +118,7 @@ class Register extends React.Component {
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" icon color="orange" textAlign="center">
+          <Header as="h1" icon color="orange" textAlign="center">
             <Icon name="signup" color="orange" />
             Register for Healthcare App
           </Header>
