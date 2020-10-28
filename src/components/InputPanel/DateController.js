3c569aae61f9d47de76c8aab130733895967f5d1
Terashima <yamanogomora@gmail.com>
2020-10-28 20:13:32 +0900

    Create return to today button UI and Functionality

--- src/components/InputPanel/DateController.js
+++ src/components/InputPanel/DateController.js
@@ -10,9 +10,9 @@ class DateController  extends React.Component {
   state = { isSameDay: true };
 
   changeDate = (direction) => {
-    const { currentDate } = this.props;
-    let newDate = new Date(currentDate);
-    newDate.setDate(currentDate.getDate() + direction);
+    const { currentDate, todaysDate } = this.props;
+    let newDate = direction !== 0 ? new Date(currentDate) : new Date(todaysDate);
+    newDate.setDate(newDate.getDate() + direction);
     this.setState({
       isSameDay: moment(newDate).isSame(moment(), 'day')
     });
@@ -24,25 +24,32 @@ class DateController  extends React.Component {
     const { currentDate } = this.props;
 
     return (
-      <Grid columns='equal' verticalAlign='middle'>
-        <Grid.Column>
-          <Button icon='angle left' onClick={() => this.changeDate(-1)} />
-        </Grid.Column>
-        <Grid.Column width={8} textAlign='center'>
-          <Header>
-            <Moment locale='ja' format='YYYY.MM.DD (ddd)'>
-              {currentDate}
-            </Moment>
-          </Header>
-        </Grid.Column>
-        <Grid.Column textAlign='right'>
-          <Button
-            disabled={isSameDay}
-            icon='angle right'
-            onClick={() => this.changeDate(1)}
-          />
-        </Grid.Column>
-      </Grid>
+      <React.Fragment>
+        <Grid columns='equal' verticalAlign='middle'>
+          <Grid.Column>
+            <Button icon='angle left' onClick={() => this.changeDate(-1)} />
+          </Grid.Column>
+          <Grid.Column width={8} textAlign='center'>
+            <Header>
+              <Moment locale='ja' format='YYYY.MM.DD (ddd)'>
+                {currentDate}
+              </Moment>
+            </Header>
+          </Grid.Column>
+          <Grid.Column textAlign='right'>
+            <Button
+              disabled={isSameDay}
+              icon='angle right'
+              onClick={() => this.changeDate(1)}
+            />
+          </Grid.Column>
+        </Grid>
+        <Button.Group widths='2' style={{ marginTop: '1rem' }}>
+          <Button>日付指定</Button>
+          <Button.Or />
+          <Button onClick={() => this.changeDate(0)}>本日</Button>
+        </Button.Group>
+      </React.Fragment>
     )
   }
 };
